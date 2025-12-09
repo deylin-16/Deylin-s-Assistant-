2ab6a6d21edb0db4a8c8692058c24c059750d5b7
deylin-16 <deylin1616@gmail.com>
2025-12-08 22:44:07 -0600

    Actualizar ©answers.js

--- plugins/©answers.js
+++ plugins/©answers.js
@@ -251,7 +251,7 @@ let handler = async (m, { conn }) => {
   if (!key) return
 
   let r = respuestas[key]
-const res = await fetch(`${kirito}/media/images/87411733_k.jpg`);
+const res = await fetch(`https://i.postimg.cc/Xv1QwhGc/1756438880305.jpg`);
   const thumb2 = Buffer.from(await res.arrayBuffer());
 const userJid = m.sender;
 
