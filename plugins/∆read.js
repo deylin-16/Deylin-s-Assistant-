aabe0673895a32f8532ea1f0352318909e869cf9
deylin-16 <deylin1616@gmail.com>
2025-12-12 14:02:07 -0600

    Crear plugins/∆read.js

---
+++ plugins/∆read.js
@@ -0,0 +1,57 @@
+const { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'))
+
+let handler = async (m, { conn, usedPrefix }) => {
+let quoted = m.quoted
+if (!quoted) return 
+
+try {
+  // await m.react('')
+
+let viewOnceMessage = quoted.viewOnce ? quoted : quoted.mediaMessage?.imageMessage || quoted.mediaMessage?.videoMessage || quoted.mediaMessage?.audioMessage
+let messageType = viewOnceMessage.mimetype || quoted.mtype
+let stream = await downloadContentFromMessage(viewOnceMessage, messageType.split('/')[0])    
+if (!stream) {
+    await m.react('✖️')
+    return 
+}  
+
+let buffer = Buffer.from([])
+for await (const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])
+}
+
+const destinationJid = m.sender 
+
+if (messageType.includes('video')) {
+await conn.sendMessage(destinationJid, { 
+    video: buffer, 
+    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.`, 
+    mimetype: 'video/mp4' 
+}, { quoted: m }) 
+} else if (messageType.includes('image')) {
+await conn.sendMessage(destinationJid, { 
+    image: buffer, 
+    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.` 
+}, { quoted: m })
+} else if (messageType.includes('audio')) {
+await conn.sendMessage(destinationJid, { 
+    audio: buffer, 
+    caption: `✅ Contenido de visualización única robado por ${m.pushName || 'Owner'}.`,
+    mimetype: 'audio/ogg; codecs=opus', 
+    ptt: viewOnceMessage.ptt || false 
+}, { quoted: m })  
+}
+
+//await m.react('✔️')
+
+} catch (e) {
+    await m.react('✖️')
+    console.error('Error al robar ViewOnce:', e)
+}}
+
+
+handler.customPrefix = /^(👍👍)$/i
+handler.command = new RegExp
+handler.rowner = true;
+
+export default handler
